b0efe040661e46a783f2caaf4291b30f0a3a676a
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-03-04 09:42:11 +0100

    change goLetter to go_letter in output

--- stimuli.js
+++ stimuli.js
@@ -13,13 +13,13 @@ function stimulusGenerator(go, nogo, isPractice) {
     
     //Go stimuli
     for (i = 0; i < 16; i ++) {
-        const newStimulus = {stimulus: goImage[i%4], data: {RorP: go, GoNoGo: "Go", position: (i%4)+1, correct_key: 32, is_practice: isPractice, goLetter: go}}
+        const newStimulus = {stimulus: goImage[i%4], data: {RorP: go, GoNoGo: "Go", position: (i%4)+1, correct_key: 32, is_practice: isPractice, go_letter: go}}
         stimuli.push(newStimulus)
     }
 
     //NoGo stimuli
     for (i = 0; i < 4; i ++) {
-        const newStimulus = {stimulus: noGoImage[i], data: {RorP: nogo, GoNoGo: "NoGo", position: i+1, correct_key: null, is_practice: isPractice, goLetter: go}}
+        const newStimulus = {stimulus: noGoImage[i], data: {RorP: nogo, GoNoGo: "NoGo", position: i+1, correct_key: null, is_practice: isPractice, go_letter: go}}
         stimuli.push(newStimulus)
     }
     return stimuli
