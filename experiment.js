1e80626b870b78b310b506504d872360acad4f1a
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-03-25 14:55:20 +0100

    add text below task

--- experiment.js
+++ experiment.js
@@ -16,13 +16,18 @@ const imagesP = ["static/images/p1.png", "static/images/p2.png", "static/images/
 const imagesR = ["static/images/r1.png", "static/images/r2.png", "static/images/r3.png", "static/images/r4.png"];
 const subjectId = jsPsych.randomization.randomID(15)
 
+const practiceStimuliFirstPart = stimulusGenerator("P", "R", 1);
+const stimuliFirstPart = stimulusGenerator("P", "R", 0);
+const practiceStimuliSecondPart= stimulusGenerator("R", "P", 1);
+const stimuliSecondPart = stimulusGenerator("R", "P", 0);
+
 /* timeline elements */
 
 const instructions = {
     type: "instructions",
     pages: [
         `<h1>${language.welcomePage.welcome}</h1><br><p>${language.welcomePage.clickNext}</p>`,
-        `<p>${language.instruction.blueStar}</p><p>${language.instruction.PorR}</p><p>${language.instruction.pressSpace}</p><p>${language.instruction.dontPress}</p><img style="width: 720px; height:405px" src=${language.instruction.img} /><p>${language.instruction.ready}</p>`
+        `<p>${language.instruction.blueStar}</p><p>${language.instruction.PorR}</p><p>${language.instruction.pressSpace}</p><p>${language.instruction.dontPress}</p><img style="width: 30rem" src=${language.instruction.img} /><p>${language.instruction.ready}</p>`
     ],
     data: {test_part: "instruction"},
     show_clickable_nav: true,
@@ -46,7 +51,7 @@ const taskStart = {
 const instructionChange = {
     type: 'instructions',
     pages: [
-    `<h2 style='color: red'>${language.instructionChange.attention}</h2><br><p>${language.instructionChange.change}</p><br><p>${language.instructionChange.pressSpace}</p><p>${language.instructionChange.dontPress}</p><img style="width: 720px; height:405px" src=${language.instructionChange.img} /><p><p>${language.instruction.ready}`
+    `<h2 style='color: red'>${language.instructionChange.attention}</h2><br><p>${language.instructionChange.change}</p><br><p>${language.instructionChange.pressSpace}</p><p>${language.instructionChange.dontPress}</p><img style="width: 30rem" src=${language.instructionChange.img} /><p><p>${language.instruction.ready}`
     ],
     data: {test_part: "instruction"},
     show_clickable_nav: true,
@@ -71,49 +76,60 @@ const endTask = {
     }
 }
   
-const fixation = {
+const fixationGoP = {
     type: 'image-keyboard-response',
     stimulus: 'static/images/start.png',
     choices: jsPsych.NO_KEYS,
     trial_duration: 1500,
-    prompt: "<p class='placeholder'>Placeholder!</p>",
+    prompt: `<p class='placeholder'>Placeholder!</p><p>Press SPACE if you see a P!</p>`,
     on_start: function(trial) {
         let message;
         let lastTrial = jsPsych.data.get().last(1).values()[0];
-        lastTrial.correct == 1 ? message = "<p class='correct'>" + `${language.feedback.correct}` + "</p>" : message = "<p class='wrong'>" + `${language.feedback.wrong}` + " </p>";
-        lastTrial.is_practice == 1 ? trial.prompt = message : trial.prompt = "<p class='placeholder'>Placeholder!</p>";
-        trial.test_part = "fixation"
+        lastTrial.correct == 1 ? message = `<p class='correct'>${language.feedback.correct}</p><p>${language.task.pressP}</p>` : message = `<p class='wrong'>${language.feedback.wrong}</p><p>${language.task.pressP}</p>`;             
+        lastTrial.is_practice == 1 ? trial.prompt = message : trial.prompt = `<p class='placeholder'>Placeholder!</p><p>${language.task.pressP}</p>`;
+         trial.test_part = "fixation"
     }
 };
 
-const practiceStimuliFirstPart = stimulusGenerator("P", "R", 1);
-const stimuliFirstPart = stimulusGenerator("P", "R", 0);
-const practiceStimuliSecondPart= stimulusGenerator("R", "P", 1);
-const stimuliSecondPart = stimulusGenerator("R", "P", 0);
+const fixationGoR = {
+    ... fixationGoP,
+    prompt: `<p class='placeholder'>Placeholder!</p><p>${language.task.pressR}</p>`,
+    on_start: function(trial) {
+        let message;
+        let lastTrial = jsPsych.data.get().last(1).values()[0];
+        lastTrial.correct == 1 ? message = `<p class='correct'>${language.feedback.correct}</p><p>${language.task.pressR}</p>` : message = `<p class='wrong'>${language.feedback.wrong}</p><p>${language.task.pressR}</p>`;             
+        lastTrial.is_practice == 1 ? trial.prompt = message : trial.prompt = `<p class='placeholder'>Placeholder!</p><p>${language.task.pressR}</p>`;
+       trial.test_part = "fixation"
+    }
+}
 
-const trial = {
+const trialGoP = {
     type: 'image-keyboard-response',
     stimulus: jsPsych.timelineVariable('stimulus'),
     data: jsPsych.timelineVariable('data'),
     choices: ['space'],
     response_ends_trial: true,
     trial_duration: 500,
-    prompt: "<p class='placeholder'>Placeholder!</p>",
+    prompt: `<p class='placeholder'>Placeholder!</p><p>${language.task.pressP}</p>`,
     on_finish: function (data) { data.test_part = "stimulus" }
 }
+
+const trialGoR = { ... trialGoP, prompt: `<p class='placeholder'>Placeholder!</p><p>${language.task.pressR}</p>`}
   
 /*************** TIMELINE ***************/
 
-const timelineElementStructure = {
-    timeline: [fixation, trial],
+const timelineElementStructureGoP = {
+    timeline: [fixationGoP, trialGoP],
     randomize_order: true,
 }
 
-const practiceFirstPart = {... timelineElementStructure, timeline_variables: practiceStimuliFirstPart, repetitions: 1}
-const practiceSecondPart = {... timelineElementStructure, timeline_variables: practiceStimuliSecondPart, repetitions: 1}
+const timelineElementStructureGoR = {... timelineElementStructureGoP, timeline: [fixationGoR, trialGoR]}
 
-const firstPart = {... timelineElementStructure, timeline_variables: stimuliFirstPart, repetitions: numberOfRepetitions}
-const secondPart = {... timelineElementStructure, timeline_variables: stimuliSecondPart, repetitions: numberOfRepetitions}
+const practiceFirstPart = {... timelineElementStructureGoP, timeline_variables: practiceStimuliFirstPart, repetitions: 1}
+const practiceSecondPart = {... timelineElementStructureGoR, timeline_variables: practiceStimuliSecondPart, repetitions: 1}
+
+const firstPart = {... timelineElementStructureGoP, timeline_variables: stimuliFirstPart, repetitions: numberOfRepetitions}
+const secondPart = {... timelineElementStructureGoR, timeline_variables: stimuliSecondPart, repetitions: numberOfRepetitions}
 
 jsPsych.data.addProperties({subject: subjectId});
 timeline.push({type: "fullscreen", fullscreen_mode: true}, instructions, practiceStart, practiceFirstPart, taskStart, firstPart, instructionChange, practiceStart, practiceSecondPart, taskStart, secondPart, endTask, {type: "fullscreen", fullscreen_mode: false});
